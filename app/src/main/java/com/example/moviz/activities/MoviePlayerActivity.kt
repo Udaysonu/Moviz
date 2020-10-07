bcc354d34b69e5faaa20885afa1e27172d88561c
udaysonu <udaysonubakka123@gmail.com>
2020-10-07 17:40:54 +0530

    adding comments for better undersanding of code

--- app/src/main/java/com/example/moviz/activities/MoviePlayerActivity.kt
+++ app/src/main/java/com/example/moviz/activities/MoviePlayerActivity.kt
@@ -21,26 +21,33 @@ class MoviePlayerActivity : AppCompatActivity() {
 
         //setting full screen view
         setFullScreen()
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_player)
-        supportActionBar?.hide()
-        val streamlink=intent.getStringExtra("MOV_LINK")
-        initExoPlayer(streamlink);
 
+        //hide the support action bar to make screen more natural
+        supportActionBar?.hide()
+
+        val streamlink=intent.getStringExtra("MOV_LINK")
+
+        //initiializes the exo-player
+        initExoPlayer(streamlink);
 
     }
 
+    //this function sets the full feature screen
     private fun setFullScreen() {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 
+
+    // this opens the exoplayer
     private fun initExoPlayer(streamlink:String) {
 
         // available in exoplayer documenentation
         simpleExoPlayer=ExoPlayerFactory.newSimpleInstance(this);
+        // add exoplayer listener it is triggered when ever the stateof the exoplayer is changed
         simpleExoPlayer.addListener(object:ExoPlayer.EventListener{
             override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
                 super.onPlayerStateChanged(playWhenReady, playbackState)
@@ -68,6 +75,8 @@ class MoviePlayerActivity : AppCompatActivity() {
 
         simpleExoPlayer.playWhenReady=true
     }
+
+    // ondestroying release the resources of exoplayer
      override fun onDestroy() {
         super.onDestroy()
         simpleExoPlayer.release()
